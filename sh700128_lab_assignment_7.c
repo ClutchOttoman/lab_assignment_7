9871f4ed14afe7665a6f92013d95e3630ab83b8a
ClutchOttoman <136831813+ClutchOttoman@users.noreply.github.com>
2023-07-07 17:44:12 -0400

    Add files via upload

---
+++ sh700128_lab_assignment_7.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void bubbleSort(int arr[], int n, int count[]) {
+  int i, j, temp,c;
+  for (i = 0; i < n - 1; i++) {
+    c = 0;
+    for (j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        c++;
+      }
+    }
+    count[i] = c;
+  }
+}
+int main() {
+  int arr[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
+  int n = 9, i;
+  int changeCount[n];
+  for (i = 0; i < n; i++) {
+    changeCount[i] = 0;
+  }
+  bubbleSort(arr, n, changeCount);
+  for (i = 0; i < n; i++) {
+    printf("Swaps for index %d: %d \n", i, changeCount[i]);
+  }
+  return 0;
+}
